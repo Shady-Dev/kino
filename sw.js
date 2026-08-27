5a34654a3496786b88c036a54d6603c6c4b04f65
Shady-Dev <19388620+Shady-Dev@users.noreply.github.com>
2026-08-27 16:22:17 +0300

    brand: bump sw cache name

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kino-v1';
+const CACHE = 'leffavuoro-v1';
 
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(clients.claim()));
